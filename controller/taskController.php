692b5f1d5ea5d989c550b8dbf14a811581af7772
khangng2611 <khang.nguyenphuc2611@hcmut.edu.vn>
2023-04-22 20:41:06 +0700

    cm to pull new

--- controller/taskController.php
+++ controller/taskController.php
@@ -1,22 +1,23 @@
 <?php
 class task extends Controller {
     function assign() {
-        if (!isset($_SESSION['id'])) $this->view("page404");
-        else $this->view("taskAssign");
+        if (isset($_SESSION['position']) && $_SESSION['position'] == 'backofficier') $this->view("taskAssign");
+        else $this->view("page404");
     }
     
     function manage() {
-        if (!isset($_SESSION['id'])) $this->view("page404");
-        else {
+        if (isset($_SESSION['position']) && $_SESSION['position'] == 'backofficier') {
             $show = $this->model('taskModel');
             $result = $show->getAllTask();
             if ($result->num_rows > 0) {
                 while ($row = $result-> fetch_assoc()) {
                     $data[] = $row;
                 }
+                $this->view("taskManage", ['result' => $data]);
             }
-            $this->view("taskManage", ['result' => $data]);
+            else $this->view("taskManage", ['msg' => 'Chưa có task nào.']);
         }
+        else $this->view("page404");    
     }
 }
 ?>
